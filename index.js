a314fb88af317fedb8d99003ff41fa81dcf3a073
Nur Nahar <hello.nurnahar@gmail.com>
2025-03-23 20:24:16 +0600

    final

--- index.js
+++ index.js
@@ -11,13 +11,14 @@ app.get("/", (req, res) => {
   res.send("hello assignment");
 });
 
-// crowdcube
-// X0frp4FWTIvEpJl6
+// 
+// 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
-  "mongodb+srv://crowdcube:X0frp4FWTIvEpJl6@cluster0.428x9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.428x9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+//   console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -38,9 +39,9 @@ async function run() {
       .collection("donatedUser");
 
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.connect();
+    // // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
